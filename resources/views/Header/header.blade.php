6df5a5045644042506aade7183a4587394a9d0b7
ZeusenAres <2060537@talnet.nl>
2022-03-11 10:21:59 +0100

    hi

---
+++ resources/views/Header/header.blade.php
@@ -0,0 +1,4 @@
+<nav class="header">
+    <link rel="stylesheet" href="css/header.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+
+</nav>
