ee198dc384ac7225ba389ac79d2d8e52e74860e5
ZeusenAres <2060537@talnet.nl>
2022-03-23 11:23:08 +0100

    changed shit

--- resources/views/Header/header.blade.php
+++ resources/views/Header/header.blade.php
@@ -2,10 +2,14 @@
     @include('Stylesheets.stylesheets')
     <a href="{{url('')}}"><img src="images/logotransparent.png" width="100" height="100" /></a>
     <a href="{{url('')}}">Home</a>
-    <p>Assortment</p>
-    @foreach($categories as $category)
-        {{ $category->name }}
-    @endforeach
+    <div class="dropdown">
+        <p>Assortment</p>
+        <div class="dropdown-content">
+            @foreach($categories as $category)
+                {{ $category->name }}
+            @endforeach
+        </div>
+    </div>
     <a href="{{url('contact')}}">Contact</a>
     <a href="{{url('cart')}}"><img src="images/shoppingcart.png" width="25" height="25"/></a>
 </nav>
